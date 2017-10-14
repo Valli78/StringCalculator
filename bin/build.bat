6b6fe420ae8109c3de98226aaafa98d6e66a242e
Valli <Valgeir.sturluson@gmail.com>
2017-10-14 06:01:24 +0000

    Sorted out clean build run scripts

--- bin/build.bat
+++ bin/build.bat
@@ -1,2 +1,2 @@
-﻿javac src/main/java/is/ru/StringCalculator/*.java -d classes 
-javac -cp "src/test/resources/junit-4.11.jar;classes/" src/test/java/is/ru/stringcalculator/CalculatorTest.java -d classes
+﻿javac src/main/java/is/ru/StringCalculator/*.java -d classes
+
